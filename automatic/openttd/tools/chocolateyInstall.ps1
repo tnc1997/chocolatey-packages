1fc102cf22fc169244635e3388cd3d18eb432062
tnc1997 <tnc1997@virginmedia.com>
2019-10-07 19:01:12 +0000

    AU: 1 updated - openttd [skip ci] https://gist.github.com/e7c96a836bcecae2d3b150fd4449ab25/09851147e6d5a2a9ae4d3db83bea4b8e1020fca0

--- automatic/openttd/tools/chocolateyInstall.ps1
+++ automatic/openttd/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@
 
 $packageName = "openttd"
 $fileType    = "exe"
-$url32       = "https://proxy.binaries.openttd.org/openttd-releases/1.9.2/openttd-1.9.2-windows-win32.exe"
-$url64       = "https://proxy.binaries.openttd.org/openttd-releases/1.9.2/openttd-1.9.2-windows-win64.exe"
-$checksum32  = "b928bf01d0c8aca65aa3823176a8072ea23cf7aecf5b83df018c15b99595a045"
-$checksum64  = "7897464889fe861cebc801bb7cbba25868ae61703d532e9a98890c1ab39c4bf5"
+$url32       = "https://proxy.binaries.openttd.org/openttd-releases/1.9.3/openttd-1.9.3-windows-win32.exe"
+$url64       = "https://proxy.binaries.openttd.org/openttd-releases/1.9.3/openttd-1.9.3-windows-win64.exe"
+$checksum32  = "ee8439e2e00b66059cfea84a82da578765991b1b57fbd79f133bc43478caf249"
+$checksum64  = "57099056f49fbb712e59079a55be10aa54f69b3a5018b84d73c014d9a3d857cb"
 
 $packageArgs = @{
   packageName    = $packageName
