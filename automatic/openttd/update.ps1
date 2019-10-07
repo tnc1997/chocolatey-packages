a54ff88af84795a53a5f8f1e0ef3ffb529004b7b
Thomas Clark <tnc1997@virginmedia.com>
2019-10-07 19:51:11 +0100

    fix: openttd automatic update script (#1)

--- automatic/openttd/update.ps1
+++ automatic/openttd/update.ps1
@@ -1,29 +1,29 @@
 import-module au
 
-$uri = "https://api.github.com/repos/openttd/openttd/releases/latest"
+$Uri = "https://api.github.com/repos/openttd/openttd/releases/latest"
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^[$]url32\s*=\s*)(`".*`")"      = "`$1`"$($Latest.URL32)`""
+            "(^[$]url64\s*=\s*)(`".*`")"      = "`$1`"$($Latest.URL64)`""
+            "(^[$]checksum32\s*=\s*)(`".*`")" = "`$1`"$($Latest.Checksum32)`""
+            "(^[$]checksum64\s*=\s*)(`".*`")" = "`$1`"$($Latest.Checksum64)`""
         }
      }
 }
 
 function global:au_GetLatest {
-    $release = Invoke-WebRequest -Uri $uri -UseBasicParsing | ConvertFrom-Json
+    $Release = Invoke-WebRequest -Uri $Uri -UseBasicParsing | ConvertFrom-Json
 
-    $version = $release.tag_name
-    $url32 = "https://proxy.binaries.openttd.org/openttd-releases/" + $version + "/openttd-" + $version + "-windows-win32.exe"
-    $url64 = "https://proxy.binaries.openttd.org/openttd-releases/" + $version + "/openttd-" + $version + "-windows-win64.exe"
+    $Version = $Release.tag_name
+    $Url32 = "https://proxy.binaries.openttd.org/openttd-releases/" + $Version + "/openttd-" + $Version + "-windows-win32.exe"
+    $Url64 = "https://proxy.binaries.openttd.org/openttd-releases/" + $Version + "/openttd-" + $Version + "-windows-win64.exe"
 
     $Latest = @{
-        URL32 = $url32
-        URL64 = $url64
-        Version = $version
+        URL32 = $Url32
+        URL64 = $Url64
+        Version = $Version
     }
 
     return $Latest
