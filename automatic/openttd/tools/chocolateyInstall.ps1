6d3ae69d11f4b6ed0c1196e923b74fb27be2af52
tnc1997 <tnc1997@virginmedia.com>
2023-10-08 12:01:32 +0000

    AU: 1 updated - openttd [skip ci] https://gist.github.com/tnc1997/e7c96a836bcecae2d3b150fd4449ab25/0663c26452298d782e1a5dde04dad15e891be189

--- automatic/openttd/tools/chocolateyInstall.ps1
+++ automatic/openttd/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@
 
 $packageName = "openttd"
 $fileType    = "exe"
-$url32       = "https://cdn.openttd.org/openttd-releases/13.3/openttd-13.3-windows-win32.exe"
-$url64       = "https://cdn.openttd.org/openttd-releases/13.3/openttd-13.3-windows-win64.exe"
-$checksum32  = "6c0722a23d0ee320b0bafff351f5b2132df38fc386fc9bb900507007fa2965bb"
-$checksum64  = "4ef745f5cb34b86a7e5d525c60b22c544c9f9da11e86698c0d12e252468084a0"
+$url32       = "https://cdn.openttd.org/openttd-releases/13.4/openttd-13.4-windows-win32.exe"
+$url64       = "https://cdn.openttd.org/openttd-releases/13.4/openttd-13.4-windows-win64.exe"
+$checksum32  = "99d9262a92c7c42fe5385b6d96f3887d077d040cb10a7b58868a77ec4058a0a6"
+$checksum64  = "dd793906db2aec7a4c78f39a65108b0f1730739e56a054f97de113d925ded20f"
 
 $packageArgs = @{
   packageName    = $packageName
